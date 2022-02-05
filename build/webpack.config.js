9544a5efdc7232a52c842bffa01bdcfe48ae3a42
che_gayhub <che_gayhub@outlook.com>
2022-02-05 13:16:14 +0800

    初始化webpack环境

---
+++ build/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, '../src', 'index.ts'),
+    output: {
+        filename: "main.js",
+    },
+    module: {
+        rules: [{
+            test: /.tsx?$/,
+            loader: 'ts-loader',
+            include: /src/,
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.json', '.js', '.jsx', '.ts', '.tsx']
+    },
+    devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        stats: 'errors-only',
+        compress: false,
+        host: 'localhost',
+        port: 8080,
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['./dist']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template/index.html'
+        })
+    ]
+};
